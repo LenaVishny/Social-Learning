5264a3977d19bba4c1641ba3d0ddee53dbabd207
lena <lenavishny3108@gmail.com>
2023-01-08 16:11:45 +0300

    postSize

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function postSize(message) {
     newMass = message.split('https')[0];
     return newMass.length;
 };
+
 console.log(postSize('Всем привет!'));
 console.log(postSize('Привет! https://github.com'));
